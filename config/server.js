8f2d716f15d438553bf18581eb70ccc90ea0b4a3
fukunaga-fv <fukunaga.s@fvs-net.co.jp>
2022-07-14 18:02:51 +0900

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '72edad14403b8798e0e755e108f6476a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3da582ae-aa78-4e35-9616-265ff2076b80'
+};
